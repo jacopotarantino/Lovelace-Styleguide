d9a3e694f73ac7afea597888f579c9ceee0c8af0
Jack <jack@ofspades.com>
2015-07-23 23:24:11 -0400

    Bootstrapped grunt plugin.

---
+++ tasks/Lovelace_Styleguide.js
@@ -0,0 +1,50 @@
+/*
+ * Lovelace-Styleguide
+ * https://github.com/jacopotarantino/Lovelace-Styleguide
+ *
+ * Copyright (c) 2015 Jack
+ * Licensed under the Creative, Commons, Attribution, Share-Alike licenses.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('Lovelace_Styleguide', 'Generates a complete styleguide for your UI components.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
